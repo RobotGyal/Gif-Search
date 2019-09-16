cdf50c1a75d99e7d77f468e2731b2258d1bc73ca
Aleia Knight <aleiaknight@gmail.com>
2019-09-16 15:36:31 -0700

    Added buttons for trending and random gifs. Need debugging

--- app.py
+++ app.py
@@ -4,16 +4,13 @@ import json
 
 app = Flask(__name__)
 
+
+#Function and Route to generate trending gifs
 @app.route('/')
 def index():
     """Return homepage."""
-    # TODO: Extract the query term from url using request.args.get()
+    # Extract the query term from url using request.args.get()
     query = request.args.get('query')
-
-    # TODO: Make 'params' dictionary containing:
-    # a) the query term, 'q'
-    # b) your API key, 'key'
-    # c) how many GIFs to return, 'limit'
 
     params = {
         'q' : query,
@@ -21,27 +18,55 @@ def index():
         'limit' : '10'
     }
 
-    # TODO: Make an API call to Tenor using the 'requests' library. For 
-    # reference on how to use Tenor, see: 
-    # https://tenor.com/gifapi/documentation
+    # Make an API call to Tenor using the 'requests' library.
     result = requests.get('https://api.tenor.com/v1/search', params=params)
 
-    # TODO: Use the '.json()' function to get the JSON of the returned response
-    # object
+    # Use the '.json()' function to get the JSON of the returned response object
     result_json = result.json()
 
-    # TODO: Using dictionary notation, get the 'results' field of the JSON,
-    # which contains the GIFs as a list
+    # Get the 'results' field of the JSON, which contains the GIFs as a list
     result_gif = result_json['results']
     
 
-    # TODO: Render the 'index.html' template, passing the list of gifs as a
+    #Render the 'index.html' template, passing the list of gifs as a
     # named parameter called 'gifs'
-
-    
     gifs = render_template('index.html', 
         query = query, 
         result_gif = result_gif)
+
+    return gifs
+
+#Function and Route to generate trending gifs
+@app.route('/trending')
+def trending():
+
+    params = {
+        'key' : 'UZ3H7US8OGBS',
+        'limit' : '10'
+    }
+
+    #API call to Tenor
+    result = requests.get('https://api.tenor.com/v1/trending', params=params)
+    trending_gifs = json.loads(result.content)['results']
+
+    gifs = render_template('index.html', trending_gifs=trending_gifs)    
+
+    return gifs
+
+#Function and Route to generate random gifs
+@app.route('/random')
+def random():
+
+    params = {
+        'key' : 'UZ3H7US8OGBS',
+        'limit' : '10'
+    }
+
+    #API call to Tenor
+    result = requests.get('https://api.tenor.com/v1/random', params=params)
+    random_gifs = json.loads(result.content)['results']
+
+    gifs = render_template('index.html', random_gifs=random_gifs)
 
     return gifs
 
